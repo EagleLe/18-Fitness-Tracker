89ce73dfda784d884f80108c383015d73e978d4e
eagle <eagle.lam.le@Gmail.com>
2021-08-20 12:37:30 -0400

    revert changes

--- routes/workoutAPI.js
+++ routes/workoutAPI.js
@@ -1,9 +1,9 @@
+const db = require("../models/workout");
 const express = require('express');
 const router = express.Router();
 const path = require("path");
-const workout = require('../models/workout');
 
-
+//get the workouts
 router.get("/api/workouts", (req, res) => {
     workout.find({}).then(dbWorkout => {
             res.json(dbWorkout);
@@ -11,12 +11,17 @@ router.get("/api/workouts", (req, res) => {
         .catch(err => {
             res.status(400).json(err);
         });
-
-
-
-
 });
-
+//Create workout
+router.post("/api/workouts/", (req, res) => {
+    console.log("Workout created?")
+    workout.create(req.body).then((dbWorkout) => {
+        res.json(dbWorkout);
+    }).catch(err => {
+        res.status(400).json(err);
+    });
+});
+// workout range
 router.get("/api/workouts/range", ({}, res) => {
     workout.find({}).then((dbWorkout) => {
         res.json(dbWorkout);
@@ -24,22 +29,16 @@ router.get("/api/workouts/range", ({}, res) => {
         res.status(400).json(err);
     });
 });
-
-
-router.post("/api/workouts/", (req, res) => {
-    workout.create(req.body).then((dbWorkout) => {
-        res.json(dbWorkout);
-    }).catch(err => {
-        res.status(400).json(err);
-    });
-});
-
+//add exercise 
 router.put("/api/workouts/:id", (req, res) => {
     workout.findByIdAndUpdate({ _id: req.params.id }, { exercises: req.body }).then((dbWorkout) => {
         res.json(dbWorkout);
     }).catch(err => {
         res.status(400).json(err);
     });
+});
+router.get("/", (req, res) => {
+    res.sendFile(path.join(__dirname, "../public/index.html"));
 });
 
 router.get("/exercise", function(req, res) {
@@ -49,6 +48,5 @@ router.get("/exercise", function(req, res) {
 router.get("/stats", function(req, res) {
     res.sendFile(path.join(__dirname, "../public/stats.html"));
 });
-
 
 module.exports = router;
